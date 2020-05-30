e1c9d02e092b43c83a8d65e88f18906480324c43
Olivier Philippe <olivier.philippe@gmail.com>
2020-05-30 21:10:05 +0200

    add config lancet

---
+++ dataCollection/config_lancet.py
@@ -0,0 +1,15 @@
+class config:
+
+    collection_tweet = "tweets_lancet"
+    list_terms = [
+        "lancetgate",
+        "#lancetgate",
+        "lancet",
+        "#lancet",
+        "The lancet",
+        "#hydroxychloroquine",
+        "hydroxychloroquine",
+        "chloroquine",
+        "#chloroquine",
+    ]
+    env_path = "./.env_ssi"
